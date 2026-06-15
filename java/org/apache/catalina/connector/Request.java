7f4a78488893cec3a7b0ad2d6d49172915d6599b
Nicole Soloveichik <nicole.s@sealsecurity.io>
2026-06-15 15:16:23 +0300

    CVE-2025-61795.patch

--- java/org/apache/catalina/connector/Request.java
+++ java/org/apache/catalina/connector/Request.java
@@ -2857,8 +2857,9 @@ public class Request implements HttpServletRequest {
             }
 
             parts = new ArrayList<>();
+            List<FileItem> items = null;
             try {
-                List<FileItem> items = upload.parseRequest(new ServletRequestContext(this));
+                items = upload.parseRequest(new ServletRequestContext(this));
                 int maxPostSize = getConnector().getMaxPostSize();
                 long postSize = 0;
                 Charset charset = getCharset();
@@ -2908,6 +2909,24 @@ public class Request implements HttpServletRequest {
                 // addParameters() will set parseFailedReason
                 checkSwallowInput();
                 partsParseException = e;
+            } finally {
+                /*
+                 * GC will delete any temporary copies of uploaded files left in the work directory but if we know that the
+                 * upload has failed then explicitly clean up now.
+                 */
+                if (!success) {
+                    parts.clear();
+                    if (items != null) {
+                        for (FileItem item : items) {
+                            try {
+                                item.delete();
+                            } catch (Throwable t) {
+                                ExceptionUtils.handleThrowable(t);
+                                log.warn(sm.getString("request.partCleanup.failed"), t);
+                            }
+                        }
+                    }
+                }
             }
         } finally {
             // This might look odd but is correct. setParseFailedReason() only
